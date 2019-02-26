7c95ed656161ab25737e0b473d46bef1dfec3cce
luoduyu <luoduyu@hotmail.com>
2019-02-26 11:04:31 +0800

    BUG修正

--- mgr/src/main/java/com/wmt/mgr/domain/redis/MessageSender.java
+++ mgr/src/main/java/com/wmt/mgr/domain/redis/MessageSender.java
@@ -1,13 +1,10 @@
 package com.wmt.mgr.domain.redis;
 
-import com.wmt.commons.constants.RedisConstants;
 import com.wmt.mgr.dao.balance.BalanceDao;
-import com.wmt.mgr.model.balance.BalanceSettingData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -32,35 +29,35 @@ public class MessageSender {
      */
     private long updatetime = -1;
 
-    /**
-     * 间隔10s检查一次 通过StringRedisTemplate对象向redis消息队列频道发布消息
-     */
-    @Scheduled(initialDelay=7000,fixedRate = 10000)
-    public void updateBalanceSetting(){
-        BalanceSettingData settingData =null;
-        try {
-            settingData = balanceDao.getGlobalSettingData();
-        } catch (Exception e) {
-           return;
-        }
-
-        try {
-
-            if(settingData == null){
-                return;
-            }
-            if(updatetime == settingData.getUpdateTime().getTime()){
-                return;
-            }
-            this.updatetime = settingData.getUpdateTime().getTime();
-
-            logger.info("余额相关设置有变化!setting={}",settingData);
-
-            stringRedisTemplate.convertAndSend(RedisConstants.REDIS_CH_BALANCE_SETTING,settingData.toString());
-
-            logger.info("余额相关设置通知完毕!setting={}",settingData.toString());
-        }catch (Exception ex){
-            logger.error(ex.getMessage(), ex);
-        }
-    }
+//    /**
+//     * 间隔10s检查一次 通过StringRedisTemplate对象向redis消息队列频道发布消息
+//     */
+//    @Scheduled(initialDelay=7000,fixedRate = 10000)
+//    public void updateBalanceSetting(){
+//        BalanceSettingData settingData =null;
+//        try {
+//            settingData = balanceDao.getGlobalSettingData();
+//        } catch (Exception e) {
+//           return;
+//        }
+//
+//        try {
+//
+//            if(settingData == null){
+//                return;
+//            }
+//            if(updatetime == settingData.getUpdateTime().getTime()){
+//                return;
+//            }
+//            this.updatetime = settingData.getUpdateTime().getTime();
+//
+//            logger.info("余额相关设置有变化!setting={}",settingData);
+//
+//            stringRedisTemplate.convertAndSend(RedisConstants.REDIS_CH_BALANCE_SETTING,settingData.toString());
+//
+//            logger.info("余额相关设置通知完毕!setting={}",settingData.toString());
+//        }catch (Exception ex){
+//            logger.error(ex.getMessage(), ex);
+//        }
+//    }
 }
